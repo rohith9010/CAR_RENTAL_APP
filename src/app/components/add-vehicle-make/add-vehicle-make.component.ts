ba63d2892a2512b06c70d4da7810f8d0ad86b4c7
rohith9010 <pattaparlarohithgoud143@gmail.com>
2024-04-22 19:37:25 +0530

    2 screens completed

--- src/app/components/add-vehicle-make/add-vehicle-make.component.ts
+++ src/app/components/add-vehicle-make/add-vehicle-make.component.ts
@@ -4,47 +4,54 @@ import { VehicleMakeService } from '../../../Services/vehicle-make.service';
 import { IVehicleMake_ } from '../../../Interfaces/IVehicleMake_';
 import { ActivatedRoute, Router } from '@angular/router';
 import {MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-vehicle-make',
   standalone: true,
-  imports: [FormsModule,ReactiveFormsModule,MatIconModule],
+  imports: [FormsModule,ReactiveFormsModule,MatIconModule,CommonModule],
   templateUrl: './add-vehicle-make.component.html',
   styleUrl: './add-vehicle-make.component.css'
 })
 export class AddVehicleMakeComponent implements OnInit{
 
-  model:IVehicleMake_={makeNo:0,name:""};
+  model:IVehicleMake_={MakeNo:0,Name:""};
 
   constructor(private route : ActivatedRoute,private MakeService:VehicleMakeService,private router:Router)
   {
 
   }
   ngOnInit(): void {
-   this.getbyid();
-}
-getbyid(){
-  const id = this.route.snapshot.params['id'];
-  if (id) {
-    this.MakeService.getById(id).subscribe(data=>
-      {this.model=data;
-     console.log(data);})
+    this.getbyid();
   }
-}
+  getbyid(){
+    const id = this.route.snapshot.params['id'];
+    if (id) {
+      this.MakeService.getById(id).subscribe(data=>{
+        this.model=data;
+        console.log(data);
+      })
+    }
+  }
 update()
 {
-  this.MakeService.updateVehicleMake(this.model).subscribe(data=>console.log(data));
-      this.router.navigate(['/vehicle_make_details']);
-    
+  this.MakeService.updateVehicleMake(this.model).subscribe(data=>{console.log(data);
+  this.router.navigate(['/vehicle_make_details'])}); 
 }
-  Save()
-  {
-     this.MakeService.AddVehicleMake(this.model).subscribe(res=>
-    console.log(res));
-     
-     this.router.navigate(['/vehicle_make_details']);
-    
-  }
+// delete(id:number):void {
+//     if (confirm('Are you sure you want to delete this item?')) {
+//       this.MakeService.deleteVehicleMake(id).subscribe(() => {
+//           console.log('Item deleted successfully');
+//           this.router.navigate(['/vehicle_make_details']);  
+//         },
+//       );
+//     }
+// }
+Save()
+{
+  this.MakeService.AddVehicleMake(this.model).subscribe(res=>{console.log(res)  
+  this.router.navigate(['/vehicle_make_details'])});
+}
 
   Clear()
   {
--- src/app/components/vehicle-make-details/vehicle-make-details.component.ts
+++ src/app/components/vehicle-make-details/vehicle-make-details.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, model } from '@angular/core';
 import {MatIconModule } from '@angular/material/icon';
 import { ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { VehicleMakeService } from '../../../Services/vehicle-make.service';
 import { IVehicleMake_ } from '../../../Interfaces/IVehicleMake_';
 import { Router } from 'express';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vehicle-make-details',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,RouterLinkActive, MatIconModule],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive, MatIconModule,FormsModule,ReactiveFormsModule],
   templateUrl: './vehicle-make-details.component.html',
   styleUrl: './vehicle-make-details.component.css'
 })
@@ -16,16 +17,35 @@ export class VehicleMakeDetailsComponent implements OnInit{
   constructor(private MakeService:VehicleMakeService){
     
   }
-  vehiclesList:any;
+  filteredList!: IVehicleMake_[];
+  searchQuery!: string ;
+  vehiclesList!:IVehicleMake_[];
   ngOnInit() {
     
 
     this.MakeService.getVehicleMake().subscribe(res=> {
       this.vehiclesList=res;
       });
+      
   }
-
-
   
-
-}
+  delete(id:number):void {
+        if (confirm('Are you sure you want to delete this item?')){
+          this.MakeService.deleteVehicleMake(id).subscribe(()=>{
+              console.log('Item deleted successfully');
+              this.ngOnInit();
+            },
+          );
+        }
+    }
+    search(): void {
+      if (this.searchQuery.trim() ==='') {
+        this.filteredList = [...this.vehiclesList];
+      } else 
+      {
+        this.filteredList = this.vehiclesList.filter(vehicle =>
+          vehicle.Name.toLowerCase().includes(this.searchQuery.trim().toLowerCase())
+        );
+      }
+    }
+  }

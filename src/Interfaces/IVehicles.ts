60a6d4b8bccdb7ad3d5628f221c4f0f4346979dc
rohith9010 <pattaparlarohithgoud143@gmail.com>
2024-06-18 17:36:39 +0530

    vehicles interface implemented successfully

--- src/Interfaces/IVehicles.ts
+++ src/Interfaces/IVehicles.ts
@@ -1,2 +1,22 @@
 export interface IVehicles {
+
+    VehicleNo:number;
+    OwnerNo:number;
+    ModelNo:number;
+    TypeNo:number;
+    Year:number;
+    Color:string;
+    FuelNo:number;
+    CapacityNo:number;
+    Mileage:number;
+    Pic:File;
+    RegistrationNo:string;
+    RegistrationState:string;
+    ChassisNo:string;
+    DailyRate:number;
+    HourlyRate:number;
+    AdditionalDailyRate:number;
+    AddtionalHourlyRate:number;
+    DeleteStatus:string;
+    
 }

a8aaaec2abaffbe35083c25071d2ed85f327f93a
rohith9010 <pattaparlarohithgoud143@gmail.com>
2024-06-18 17:58:22 +0530

    implemented vehicles service code

--- src/Services/VehiclesService/Vehicles.service.ts
+++ src/Services/VehiclesService/Vehicles.service.ts
@@ -1,10 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IVehicles } from '../../Interfaces/IVehicles';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehiclesService {
 
-constructor() { }
+  private apiUrl = 'https://localhost:7028/api/Vehicles';
 
+constructor(private http : HttpClient) { }
+
+  GetVehicles():Observable<IVehicles[]>
+  {
+    return this.http.get<IVehicles[]>(this.apiUrl);
+  }
+  AddVehicle(vehicle: IVehicles): Observable<IVehicles> {
+    return this.http.post<IVehicles>(this.apiUrl, vehicle);
+  }
+  GetVehiclesById(id: number):Observable<IVehicles> {
+    return this.http.get<IVehicles>(`${this.apiUrl}/${id}`);
+  }
+  UpdateVehicle(vehicle: IVehicles ):Observable<IVehicles>{
+    return this.http.put<IVehicles>(`${this.apiUrl}/${vehicle.VehicleNo}`,vehicle);
+  }
+  DeleteVehicle(id: number): Observable<IVehicles> {
+    return this.http.delete<IVehicles>(`${this.apiUrl}/${id}`);
+  }
 }

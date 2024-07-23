296a77a6b63e51fc86c26a22eed6e08654f49776
T Rahul Kumar <163146344+Rahul792844@users.noreply.github.com>
2024-07-23 18:53:28 +0530

    Add Driver and Driver Details Are Complted

--- src/app/components/DriverDetails/Driver-Details/Driver-Details.component.ts
+++ src/app/components/DriverDetails/Driver-Details/Driver-Details.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { IDriver } from '../../../../Interfaces/IDriver';
+import { DriverService } from '../../../../Services/DriverService/Driver.service';
+// import { ICountry } from '../../../../Interfaces/ICountry';
+// import { CountryService } from '../../../../Services/CountriesService/Country.service';
+// import { IState } from '../../../../Interfaces/IState';
+// import { StateserviceService } from '../../../../Services/StateService/stateservice.service';
+// import { ICity } from '../../../../Interfaces/ICity';
+// import { CitiesService } from '../../../../Services/CityService/Cities.service';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-Driver-Details',
+  standalone:true,
+  imports:[
+   FormsModule,
+   RouterLink,
+   ReactiveFormsModule, 
+   RouterOutlet,
+   RouterLinkActive,
+  ],
   templateUrl: './Driver-Details.component.html',
   styleUrls: ['./Driver-Details.component.css']
 })
 export class DriverDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Driverservice:DriverService,
+    private router:ActivatedRoute
+   ) { }
+
+   filteredDriverList!: IDriver[];
+  searchQuery!: string ;
+  driverList!:IDriver[];
 
   ngOnInit() {
+    this.GetDriver();
   }
 
+  GetDriver()
+  {
+    this.Driverservice.GetDriver().subscribe((res:IDriver[])=>{
+      this.driverList=res;
+      console.log(res);
+    })
+  }
+  search(): void {
+    if (this.searchQuery.trim() ==='') {
+      this.filteredDriverList = [...this.driverList];
+    } else 
+    {
+      this.filteredDriverList = this.driverList.filter(driver =>
+        driver.DriverName.toLowerCase().includes(this.searchQuery.trim().toLowerCase())
+      );
+    }
+  }
+  delete(id:number):void {
+    if (confirm('Are you sure you want to delete this item?')){
+      this.Driverservice.DeleteDriver(id).subscribe(()=>{
+          console.log('Item deleted successfully');
+          this.ngOnInit();
+        },
+      );
+    }
+}
 }

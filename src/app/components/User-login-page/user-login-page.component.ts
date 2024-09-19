d6fb30904f46226ea611ac1a710c0994afcb9bec
bhargav-chokkam <adamapranay12@gmail.com>
2024-09-19 19:25:35 +0530

    user page

--- src/app/components/User-login-page/user-login-page.component.ts
+++ src/app/components/User-login-page/user-login-page.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-login-page',
   templateUrl: './user-login-page.component.html',
   styleUrls: ['./user-login-page.component.css']
 })
-export class UserLoginPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class UserLoginPageComponent {
 
 }
